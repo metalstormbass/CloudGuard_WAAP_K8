caf2f7f6c35d13ff3d2849c9ad7f7dcce7eebb6a
Michael Braun <michaelbraunbass@gmail.com>
2020-10-01 08:57:10 -0600

    testing helm

--- helm.tf
+++ helm.tf
@@ -1,6 +1,6 @@
 #Provider for Helm
 provider "helm" {
-    kubernetes" {
+    kubernetes {
     host                   = azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.vuln_k8_cluster.kube_config.0.client_key)
