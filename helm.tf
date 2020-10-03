cb84e14edfac85efe26f5e7d3c4eb5257760619b
metalstormbass <michaelbraunbass@gmail.com>
2020-10-03 13:35:29 -0600

    CP WAAP Helm Test

--- helm.tf
+++ helm.tf
@@ -12,7 +12,7 @@ provider "helm" {
 
 resource "helm_release" "cp_waap_helm" {
     name  = "cp_waap_helm"
-    chart = "https://github.com/metalstormbass/cp_waap_helm/blob/main/CP_WAAP_Helm/CP_WAAP_Helm-0.1.0.tgz"
+    chart = "https://raw.githubusercontent.com/metalstormbass/cp_waap_helm/main/CP_WAAP_Helm/CP_WAAP_Helm-0.1.0.tgz"
     namespace = kubernetes_namespace.vulnk8_namespace.metadata.0.name
     version    = "3.3.0"
     set {
