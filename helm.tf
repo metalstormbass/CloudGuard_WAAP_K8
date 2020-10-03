526867c9cd4f10d7d62aba0bbacc0d08479a77b3
metalstormbass <michaelbraunbass@gmail.com>
2020-10-03 13:11:51 -0600

    CP WAAP Helm Test

--- helm.tf
+++ helm.tf
@@ -10,10 +10,13 @@ provider "helm" {
 }
 
 
-resource "helm_release" "nginx_ingress" {
-    name      = "ingress-nginx"
-    chart     = "ingress-nginx"
-    repository      = "https://kubernetes.github.io/ingress-nginx"
+resource "helm_release" "cp_waap_helm" {
+    name  = "cp_waap_helm"
+    chart = "https://github.com/metalstormbass/cp_waap_helm/blob/main/CP_WAAP_Helm/CP_WAAP_Helm-0.1.0.tgz?raw=true"
     namespace = kubernetes_namespace.vulnk8_namespace.metadata.0.name
     version    = "3.3.0"
+    set {
+    name  = "token"
+    value = var.token
+  }
 }
