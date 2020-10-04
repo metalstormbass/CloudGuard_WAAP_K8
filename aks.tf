9ebe050fcaae7679be8dfbb19122b327e1dc129d
metalstormbass <michaelbraunbass@gmail.com>
2020-10-03 21:33:00 -0600

    update

--- aks.tf
+++ aks.tf
@@ -133,7 +133,7 @@ resource "kubernetes_ingress" "nginx_ingress" {
   spec {
      
     rule {
-      host = "test.test"
+      host = "40.125.99.100"
       http {
         path {
           backend {
