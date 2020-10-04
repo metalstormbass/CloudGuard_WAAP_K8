e8bd9bd8d512723fd5f0e3224e1c5dea1aa7e5b2
metalstormbass <michaelbraunbass@gmail.com>
2020-10-04 09:49:52 -0600

    update

--- aks.tf
+++ aks.tf
@@ -138,6 +138,7 @@ resource "kubernetes_ingress" "nginx_ingress" {
             service_port = 80
           }
     rule {
+      host = "vulnerablekubewestus2.cloudapp.azure.com"
       http {
         path {
           backend {
