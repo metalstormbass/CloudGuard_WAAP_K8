6c6ff5f18b59aa715c0967910c8201da7766142e
Michael Braun <michaelbraunbass@gmail.com>
2020-10-07 10:47:08 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
       backend "remote" {
          # The name of your Terraform Cloud organization.
-         organization = "ENTER_ORGANIZATION_HERE"
+         organization = "REPLACE.ORGANIZATION"
 
          # The name of the Terraform Cloud workspace to store Terraform state files in.
          workspaces {
-           name = "ENTER_WORKSPACE_HERE"
+           name = "REPLACE.WORKSPACE"
          }
        }
      }
